4081c84e9e4bf6443798f83410a7e95be0aa14d2
atefBB <atef.bettaib@gmail.com>
2016-08-14 12:20:43 +0100

    Add A brand image to navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,70 +23,74 @@
 			<div class="page-header">
 				<nav class="navbar navbar-default navbar-fixed-top" 
 				 	 role="navigation">
-					<div class="navbar-header">
-						<button type="button" 
-								class="navbar-toggle" 
-								data-toggle="collapse" 
-								data-target="#bs-example-navbar-collapse-1">
-							 <span class="sr-only">Toggle navigation</span>
-							 <span class="icon-bar"></span>
-							 <span class="icon-bar"></span>
-							 <span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand" 
-							href="#">
-							Atef Ben Ali
-						</a>
-					</div>
-					<div class="collapse navbar-collapse" 
-						 id="bs-example-navbar-collapse-1">
-						<ul class="nav navbar-nav">
-							<li class="active">
-								<a href="#first-link">Link</a>
-							</li>
-							<li>
-								<a href="#second-link">Link</a>
-							</li>
-							<li class="dropdown">
-								<a href="#dropdown" 
-								 	class="dropdown-toggle" 
-								 	data-toggle="dropdown">
-								 	Dropdown
-								 	<strong class="caret"></strong>
-								</a>
-								<ul class="dropdown-menu">
-									<li>
-										<a href="#action">
-											Action
-										</a>
-									</li>
-									<li>
-										<a href="#another-action">
-											Another action
-										</a>
-									</li>
-									<li>
-										<a href="#something-else">
-											Something else here
-										</a>
-									</li>
-									<li class="divider">
-									</li>
-									<li>
-										<a href="#separated-link">
-											Separated link
-										</a>
-									</li>
-									<li class="divider">
-									</li>
-									<li>
-										<a href="#sepated-link">
-											One more separated link
-										</a>
-									</li>
-								</ul>
-							</li>
-						</ul>
+				 	<div class="container"> 
+						<div class="navbar-header">
+							<button type="button" 
+									class="navbar-toggle" 
+									data-toggle="collapse" 
+									data-target="#bs-example-navbar-collapse-1">
+								 <span class="sr-only">Toggle navigation</span>
+								 <span class="icon-bar"></span>
+								 <span class="icon-bar"></span>
+								 <span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" 
+								href="#brand">
+								<img alt="Brand" 
+									 src="myblog-brand.png" 
+									 width="30">
+							</a>
+						</div>
+						<div class="collapse navbar-collapse" 
+							 id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li>
+									<a href="#first-link">Link</a>
+								</li>
+								<li>
+									<a href="#second-link">About Me</a>
+								</li>
+								<li class="dropdown">
+									<a href="#dropdown" 
+									 	class="dropdown-toggle" 
+									 	data-toggle="dropdown">
+									 	Dropdown
+									 	<strong class="caret"></strong>
+									</a>
+									<ul class="dropdown-menu">
+										<li>
+											<a href="#action">
+												Action
+											</a>
+										</li>
+										<li>
+											<a href="#another-action">
+												Another action
+											</a>
+										</li>
+										<li>
+											<a href="#something-else">
+												Something else here
+											</a>
+										</li>
+										<li class="divider">
+										</li>
+										<li>
+											<a href="#separated-link">
+												Separated link
+											</a>
+										</li>
+										<li class="divider">
+										</li>
+										<li>
+											<a href="#sepated-link">
+												One more separated link
+											</a>
+										</li>
+									</ul>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</nav>
 				<br><br>
