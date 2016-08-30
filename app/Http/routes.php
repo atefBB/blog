80f346ec0a04ace950d5d2bb757096346dd43e82
atefBB <atef.bettaib@gmail.com>
2016-08-30 20:03:08 +0100

    blog

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('ffmpeg', function(){
+	
+	$ffmpeg = FFMpeg\FFMpeg::create(array(
+			'ffmpeg.binaries'  => 'C:/FFmpeg/bin/ffmpeg.exe',
+			'ffprobe.binaries' => 'C:/FFmpeg/bin/ffprobe.exe',
+			'timeout'          => 3600, // The timeout for the underlying process
+			'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
+		));
+	$video = $ffmpeg->open('https://www.youtube.com/watch?v=WOg_XPJR_Mk');
+	$video->save(new FFMpeg\Format\Video\X264(), 'export-x264.mp4');
+});
